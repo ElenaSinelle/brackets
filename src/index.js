2ccc69d75bd161bd818ab03535a99aa607f92e68
Elena Sinelle <elle.sinelle@gmail.com>
2022-08-12 18:15:44 +0300

    fix: update task solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
   let strArr = str.split('');
+  for (let item of strArr) {
+    if (item === ',' || item === ' ') {
+      strArr.splice(i, 1);
+        i = i - 1;
+    }
+  } 
   for (let i = 0; i < strArr.length; i++) {
     for (let item of bracketsConfig) {
       if (strArr[i] === item[0] && strArr[i+1] === item[1]) {
